3a9c1260fe438425306f43bc0171428b7ffaed90
tugascrown204 <gajdertevet@hotmail.com>
2025-10-04 20:22:23 +0330

    feat: initial commit for src/components/Button.js

---
+++ src/components/Button.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Button = ({ label, onClick, disabled = false }) => {
+    return (
+        <button onClick={onClick} disabled={disabled}>
+            {label}
+        </button>
+    );
+};
+
+export default Button;
